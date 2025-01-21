cad2fcae91bbf6ab7ff6964b924df13b012cf7e3
Juan Camilo Mendez <Juanmendez17t@gmail.com>
2025-01-21 16:54:52 -0500

    Feature: Vercel.Json

--- vite.config.js
+++ vite.config.js
@@ -17,6 +17,9 @@ export default defineConfig({
             },
         }),
     ],
+    build: {
+        OutDir: 'public/build',
+    },
     resolve: {
         alias: {
             '@': '/resources/js',
